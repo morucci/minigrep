e0c0195982db9e6ea67c7e8de38986b7cbfd4339
Fabien Boucher <fboucher@redhat.com>
2023-10-27 15:19:19 +0000

    up

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,11 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+    let config = Config::build(&args).unwrap_or_else(|_| {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
-    if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+    if let Err(_) = minigrep::run(config) {
         process::exit(1);
     };
 }
