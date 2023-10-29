6c18f1a3a9998dd38edba390c43df718c3b587bd
Fabien Boucher <fboucher@redhat.com>
2023-10-29 09:21:10 +0000

    wrap up the first book exercise

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|_| {
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments {err}");
         process::exit(1);
     });
 
-    if let Err(_) = minigrep::run(config) {
+    if let Err(err) = minigrep::run(config) {
+        eprintln!("Application error: {err}");
         process::exit(1);
     };
 }
